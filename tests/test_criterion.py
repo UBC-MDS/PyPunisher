53d02a4999ecb7e494bb187b6de790662d8db876
Jill Cates <cates.jill@gmail.com>
2018-02-13 19:42:16 -0800

    Import criterion file in test_criterion.py

--- tests/test_criterion.py
+++ tests/test_criterion.py
@@ -5,16 +5,11 @@ Criterion Tests
 import os
 import sys
 import pytest
-import numpy as np
-import pandas as pd
-
 
 sys.path.insert(0, os.path.abspath("."))
 sys.path.insert(0, os.path.abspath("../"))
 
-import os
-import sys
-import pytest
+import criterion
 
 def test_aic():
   print('test goes here')
