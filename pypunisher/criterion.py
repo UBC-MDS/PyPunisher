7d3114d46c2ed8b44ca10a84157615eb8f81d392
TariqAHassan <tariqaahassan@gmail.com>
2018-02-13 18:28:38 -0800

    Add scaffold.

---
+++ pypunisher/criterion.py
@@ -0,0 +1,38 @@
+"""
+
+     Information Criterion
+     ~~~~~~~~~~~~~~~~~~~~~
+
+"""
+
+
+def aic(model):
+    """Compute the Akaike Information Criterion (AIC)
+
+    Args:
+        model (sklearn model object): ...
+
+    Returns:
+        float: ...
+
+    References:
+        * https://en.wikipedia.org/wiki/Akaike_information_criterion
+
+    """
+    pass
+
+
+def bic(model):
+    """Compute the Bayesian Information Criterion (BIC)
+
+    Args:
+        model (sklearn model object): ...
+
+    Returns:
+        float: ...
+
+    References:
+        * https://en.wikipedia.org/wiki/Bayesian_information_criterion
+
+    """
+    pass
