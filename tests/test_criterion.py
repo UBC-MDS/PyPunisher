8fc7e38219a1b24c26c4b51796eed013cfa79a39
ap26793 <zi.avi7@gmail.com>
2018-02-25 14:26:52 -0800

    Added initial tests

--- tests/test_criterion.py
+++ tests/test_criterion.py
@@ -14,5 +14,11 @@ import criterion
 def test_aic():
   print('test goes here')
 
+def test_model_type():
+  # Check if the model passed into AIC is of correct type
+
+def test_lambda_type():
+  # Check if the lambda parameter is a number
+
 def test_bic():
   print('test goes here')
