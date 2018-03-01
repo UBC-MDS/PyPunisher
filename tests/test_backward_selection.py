25a7505761a8ba74a5207aca54113c0e8bc48a72
Jill Cates <cates.jill@gmail.com>
2018-03-01 15:13:43 -0800

    Create tests for backward methods

--- tests/test_backward_selection.py
+++ tests/test_backward_selection.py
@@ -63,6 +63,19 @@ def test_sklearn_model_methods():
       with pytest.raises(AttributeError):
         BackwardSelection(model, method, verbose=True)
 
+def test_backward_attributes(selection):
+  '''
+  Check attribute types of that attributes of backward methods are correct 
+  '''
+  msg = "Number of features must be a positive integer or float"
+  with pytest.raises(AttributeError, match=msg):
+    selection.backward(n_features=-0.5)
+
+  msg = "n_features must be None for min_change to work"
+  with pytest.raises(AttributeError, match=msg):
+    selection.backward(n_features=0.5, min_change=0.5)
+
+
 # test output
 def test_output_type(selection):
   '''
