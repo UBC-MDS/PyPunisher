b7bb421991614292d75d6dbdb0b4bbc0dda2e5fa
TariqAHassan <tariqaahassan@gmail.com>
2018-03-03 08:34:42 -0800

    Update Docstrings and slightly improve formatting

--- tests/test_criterion.py
+++ tests/test_criterion.py
@@ -1,5 +1,8 @@
 """
-Criterion Tests
+
+    Criterion Tests
+    ~~~~~~~~~~~~~~~
+
 """
 
 import os
@@ -9,13 +12,12 @@ import pytest
 sys.path.insert(0, os.path.abspath("."))
 sys.path.insert(0, os.path.abspath("../"))
 
-import numpy as np
 import statsmodels.api as sm
 from pypunisher.metrics.criterion import aic, bic
 from sklearn.linear_model import LinearRegression
 from tests._test_data import X_train, y_train
 
-COMP_TOLERANCE = 0.5  # comaprision tolerance between floats
+COMP_TOLERANCE = 0.5  # comparision tolerance between floats
 
 # -----------------------------------------------------------------------------
 # Setup
@@ -26,7 +28,7 @@ res = sm_model.fit()
 sm_aic = res.aic
 sm_bic = res.bic
 
-model = LinearRegression()
+sk_model = LinearRegression()
 
 
 # -----------------------------------------------------------------------------
@@ -35,7 +37,10 @@ model = LinearRegression()
 
 
 def test_metric_model_parm():
-    for kind in ("invalid", model):
+    """Test that the `model` param in `aic()` and `bic()`
+    will raise a TypeError when passed something other
+    than a sk-learn model."""
+    for kind in ("invalid", sk_model):
         for metric in (aic, bic):
             if isinstance(kind, str):
                 with pytest.raises(TypeError):
@@ -50,13 +55,16 @@ def test_metric_model_parm():
 
 
 def test_metric_data_parm():
+    """Test that the `data` param in `aic()` and `bic()`
+    will raise a TypeError when passed something other
+    than an ndarray."""
     for kind in ("invalid", X_train):
         for metric in (aic, bic):
             if isinstance(kind, str):
                 with pytest.raises(TypeError):
-                    metric(model, data=kind)
+                    metric(sk_model, data=kind)
             else:
-                metric(model, data=kind)
+                metric(sk_model, data=kind)
 
 
 # -----------------------------------------------------------------------------
@@ -65,8 +73,10 @@ def test_metric_data_parm():
 
 
 def test_metric_output():
+    """Test that both metrics (`aic()` and `bic()` return
+    floating point numbers."""
     for metric in (aic, bic):
-        assert isinstance(metric(model, data=X_train), float)
+        assert isinstance(metric(sk_model, data=X_train), float)
 
 
 # -----------------------------------------------------------------------------
@@ -75,6 +85,9 @@ def test_metric_output():
 
 
 def test_metric_output_value():
+    """Test that the actual AIC and BIC values computed by
+    our functions match that computed by a well-respected
+    statistical library in Python (StatsModels)."""
     for metric, comparision in zip((aic, bic), (sm_aic, sm_bic)):
-        ours = metric(model, data=X_train)
+        ours = metric(sk_model, data=X_train)
         ours == pytest.approx(comparision, abs=COMP_TOLERANCE)
