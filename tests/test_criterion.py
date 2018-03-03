9590353370ded3ed1311c0f5e20f72374118f165
TariqAHassan <tariqaahassan@gmail.com>
2018-03-03 00:10:57 -0800

    Add test for actual AIC and BIC values

--- tests/test_criterion.py
+++ tests/test_criterion.py
@@ -9,9 +9,18 @@ import pytest
 sys.path.insert(0, os.path.abspath("."))
 sys.path.insert(0, os.path.abspath("../"))
 
+import numpy as np
 from pypunisher.metrics.criterion import aic, bic
 from sklearn.linear_model import LinearRegression
-from tests._test_data import X_train
+from tests._test_data import X_train, y_train
+import statsmodels.api as sm
+
+sm_model = sm.OLS(y_train, X_train)
+res = sm_model.fit()
+sm_aic = res.aic
+sm_bic = res.bic
+
+COMP_TOLERANCE = 0.5  # comaprision tolerance between floats
 
 model = LinearRegression()
 
@@ -53,3 +62,13 @@ def test_metric_data_parm():
 def test_metric_output():
     for metric in (aic, bic):
         assert isinstance(metric(model, data=X_train), float)
+
+
+# -----------------------------------------------------------------------------
+# Output Value (Compare against the Stats Models Package).
+# -----------------------------------------------------------------------------
+
+def test_metric_output_value():
+    for metric, comparision in zip((aic, bic), (sm_aic, sm_bic)):
+        ours = metric(model, data=X_train)
+        ours == pytest.approx(comparision, abs=COMP_TOLERANCE)
