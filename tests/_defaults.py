6de5c2b18b845fe725ce62f40e50018ef78c7ebc
TariqAHassan <tariqaahassan@gmail.com>
2018-03-08 00:41:59 -0800

    Rename DEFAULT_FORWARD_SELECTION_PARAMS to DEFAULT_SELECTION_PARAMS

--- tests/_defaults.py
+++ tests/_defaults.py
@@ -7,7 +7,7 @@
 from sklearn.linear_model import LinearRegression
 from tests._test_data import X_train, y_train, X_val, y_val
 
-DEFAULT_FORWARD_SELECTION_PARAMS = {
+DEFAULT_SELECTION_PARAMS = {
     'model': LinearRegression(), 'X_train': X_train, 'y_train': y_train,
     'X_val': X_val, 'y_val': y_val, 'verbose': False, 'criterion': None
 }
