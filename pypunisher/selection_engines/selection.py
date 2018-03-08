b7bc417deb63c410d76ad49dc0aabd9b0131fc87
TariqAHassan <tariqaahassan@gmail.com>
2018-03-08 02:40:59 -0800

    Add ToDo

--- pypunisher/selection_engines/selection.py
+++ pypunisher/selection_engines/selection.py
@@ -55,7 +55,7 @@ class Selection(object):
         array_check(self)
 
         self._verbose = verbose
-        self._criterion = criterion
+        self._criterion = criterion  # ToDo: not implemented.
         self._n_features = get_n_features(X_train)
 
     def _fit_and_score_forward(self, S, include):
