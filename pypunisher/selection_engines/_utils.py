5919c10f164c8e07f926a9585d79a3ab10b06c93
TariqAHassan <tariqaahassan@gmail.com>
2018-03-07 22:40:42 -0800

    Add `model_check()`

--- pypunisher/selection_engines/_utils.py
+++ pypunisher/selection_engines/_utils.py
@@ -84,3 +84,23 @@ def array_check(self):
     for i in ("_X_train", '_y_train', '_X_val', '_y_val'):
         if not isinstance(getattr(self, i), np.ndarray):
             raise TypeError("{} must be a ndarray.".format(i[1:]))
+
+
+def model_check(model):
+    """Check that `model` is a sklearn model
+    and that it contains `fit`, `predict` and `score` methods.
+
+    Args:
+        model : sklearn model
+            Any sklearn model
+
+    """
+    if 'sklearn' not in str(type(model)):
+        raise TypeError(
+            "`model` is not a sklearn model"
+        )
+    for attr in ('fit', 'predict', 'score'):
+        if not hasattr(model, attr):
+            raise AttributeError(
+                "`model` does not contain an {} method".format(attr)
+            )
