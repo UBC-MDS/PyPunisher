1aeb34542948510fdd60b789b593cbe9f63c05af
TariqAHassan <tariqaahassan@gmail.com>
2018-03-09 21:42:36 -0800

    Add version information

--- pypunisher/__init__.py
+++ pypunisher/__init__.py
@@ -1,2 +1,5 @@
 from pypunisher.metrics.criterion import aic, bic
 from pypunisher.selection_engines.selection import Selection
+
+__version_info__ = (3, 0, 0)
+__version__ = '.'.join(map(str, __version_info__))
