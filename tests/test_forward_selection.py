1c257a1814f062465ca9ebdc5a80489fda6ea00f
TariqAHassan <tariqaahassan@gmail.com>
2018-03-16 23:59:09 -0700

    Refactor

--- tests/test_forward_selection.py
+++ tests/test_forward_selection.py
@@ -13,7 +13,7 @@ sys.path.insert(0, os.path.abspath("."))
 sys.path.insert(0, os.path.abspath("../"))
 
 from tests._wrappers import forward
-from pypunisher._example_data import X_train
+from pypunisher.example_data._example_data import X_train
 
 
 # -----------------------------------------------------------------------------
