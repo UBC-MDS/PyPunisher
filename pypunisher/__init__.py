73918fc8949613daac061c3bfdf460f13867a35e
TariqAHassan <tariqaahassan@gmail.com>
2018-03-17 02:26:41 -0700

    Bump version

--- pypunisher/__init__.py
+++ pypunisher/__init__.py
@@ -13,5 +13,5 @@ Criterion, both of which punish complex models.
 from pypunisher.metrics.criterion import aic, bic
 from pypunisher.selection_engines.selection import Selection
 
-__version_info__ = (3, 0, 0)
+__version_info__ = (4, 0, 0)
 __version__ = '.'.join(map(str, __version_info__))
